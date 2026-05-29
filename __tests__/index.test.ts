522fc3a7e5d2c7fea8e6a37cdb738be41eba17ed
Parro <29497+Parro@users.noreply.github.com>
2026-05-29 09:52:19 +0200

    chore: Correct lint

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -8,7 +8,7 @@ import * as main from '../src/main';
 // Mock the action's entrypoint
 const runMock = jest
   .spyOn(main, 'run')
-  .mockImplementation(() => Promise.resolve());
+  .mockImplementation(async () => Promise.resolve());
 
 describe('index', () => {
   it('calls run when imported', async () => {
